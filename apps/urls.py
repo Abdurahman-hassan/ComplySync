354e4e0592c631b3dfc65e1d09f1ecb7085c1288
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-04-29 01:57:58 +0300

    update urls

--- apps/urls.py
+++ apps/urls.py
@@ -2,10 +2,36 @@ from django.urls import path, include
 from drf_spectacular.views import SpectacularRedocView, SpectacularSwaggerView
 from drf_spectacular.views import SpectacularAPIView as SpectacularView
 from drf_spectacular.utils import extend_schema
+from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenBlacklistView
+
+from apps.users.views import BulkUserCreateAPIView
+
+# from apps.users.views import CustomUserViewSet
+#
+# router = DefaultRouter()
+# router.register(r'users', CustomUserViewSet)
 
 
 urlpatterns = [
-    path("", include("apps.authentications.urls")),
+    path('bulk-create/', BulkUserCreateAPIView.as_view(), name='bulk_create'),
+    # path('', include(router.urls)),
+    # path("", include("apps.authentications.urls")),
+    path('auth/', include('djoser.urls')),
+    # /token/login/,  /token/logout/
+    path('auth/', include('djoser.urls.authtoken')),
+    # path('api-token-auth', obtain_auth_token, name='api_token_auth'),
+
+    # # """##################### rest_framework_simplejwt urls #####################"""
+    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('api/token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
+
+    # path('auth/jwt/create/',CustomTokenObtainPairView.as_view(),name='custom_jwt_create'),
+    # path('auth/', include('djoser.urls')),
+    # path('auth/', include('djoser.urls.jwt')),
+
 ]
 
 
