bf83cb5bcedca8bfaa3b6c307a4fc35f87d36430
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-04-29 01:58:10 +0300

    update urls

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+admin.site.site_header = "ComplySync"
+admin.site.index_title = 'Admin'
 
 # include apps
 urlpatterns = [
@@ -23,3 +25,5 @@ if settings.DEBUG is True:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
