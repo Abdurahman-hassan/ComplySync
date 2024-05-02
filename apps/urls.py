6a2cd6807ce816689b72a9a4c85b5b4569da84d2
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-05-02 08:51:51 +0300

    update urls

--- apps/urls.py
+++ apps/urls.py
@@ -1,25 +1,23 @@
 from django.urls import path, include
-from drf_spectacular.views import SpectacularRedocView, SpectacularSwaggerView
-from drf_spectacular.views import SpectacularAPIView as SpectacularView
 from drf_spectacular.utils import extend_schema
-from rest_framework.authtoken.views import obtain_auth_token
+from drf_spectacular.views import SpectacularAPIView as SpectacularView
+from drf_spectacular.views import SpectacularRedocView, SpectacularSwaggerView
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenBlacklistView
 
+from apps.campaigns.views import CampaignViewSet
+from apps.groups.views import GroupViewSet
+from apps.policies.views import PolicyViewSet, LanguageViewSet
 from apps.users.views import BulkUserCreateAPIView
 
-# from apps.users.views import CustomUserViewSet
-#
-# router = DefaultRouter()
-# router.register(r'users', CustomUserViewSet)
-
+router = DefaultRouter()
+router.register(r'groups', GroupViewSet)
+router.register(r'policies', PolicyViewSet)
+router.register(r'languages', LanguageViewSet)
+router.register(r'campaigns', CampaignViewSet)
 
 urlpatterns = [
     path('bulk-create/', BulkUserCreateAPIView.as_view(), name='bulk_create'),
-    # path('', include(router.urls)),
-    # path("", include("apps.authentications.urls")),
     path('auth/', include('djoser.urls')),
-    # /token/login/,  /token/logout/
     path('auth/', include('djoser.urls.authtoken')),
     # path('api-token-auth', obtain_auth_token, name='api_token_auth'),
 
@@ -49,3 +47,5 @@ urlpatterns += [
     ),
     path("docs/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
+
+urlpatterns += router.urls
