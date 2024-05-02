85581cbff8f9c847a3e043cb200dc418552240be
mojtaba <mojtaba.mohamed187@gmail.com>
2024-05-02 14:47:09 +0300

    move layout logic from home to layout

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,28 +1,8 @@
 import React from'react';
-import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
 
 const Home = () => {
 
-    const navigate = useNavigate();
-
-    const logout = async () => {
-    const authToken = localStorage.getItem('authToken');
-    const config = {
-        headers: { Authorization: `Token ${authToken}` }
-    };
-    await axios.post('http://127.0.0.1:8000/api/auth/token/logout/', null, config);
-    localStorage.removeItem('authToken');
-    navigate('/');
-    }
-
-    return (
-        <div>
-            <h1>Welcome to ComplySync!</h1>
-            <Link to="/emailuploader">Upload users</Link>
-            <button onClick={logout}>Logout</button>
-        </div>
-    );
+    return <div><h2>Home Page</h2></div>;
 }
 
 export default Home;
