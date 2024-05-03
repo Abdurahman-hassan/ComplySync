7eabb2450988dfc6162beab808f2668fa4ff0e41
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-05-03 11:05:18 +0300

    update storage file

--- config/utils/storages.py
+++ config/utils/storages.py
@@ -4,93 +4,64 @@ import os
 INSTALLED_APPS += ["collectfast", "storages"]
 
 TYPE_STORAGE = env("TYPE_STORAGE", default="local")
+STATICFILES_LOCATION = 'static'
 
 if TYPE_STORAGE == "s3":
-    # STORAGES = {
-    #     "default": {
-    #         "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
-    #         "OPTIONS": {
-    #             "location": "media",
-    #             "file_overwrite": False,
-    #         },
-    #     },
-    #     "staticfiles": {
-    #         "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
-    #         "OPTIONS": {
-    #             "location": "static",
-    #             "default_acl": "None",
-    #         },
-    #     },
-    # }
-
-    # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
-    AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
-    # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
-    AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
-    # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+    # AWS S3 credentials and bucket settings
     AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
-    # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+    AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
+    AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME", default=None)
+    AWS_S3_SIGNATURE_VERSION = "s3v4"
+
+    # AWS S3 custom domain
+    AWS_S3_CUSTOM_DOMAIN = env("AWS_S3_CUSTOM_DOMAIN", default=f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com")
+
+    # URL settings
+    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+
+    # Static and media storage settings
+    STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+    # AWS settings for object storage
+    AWS_DEFAULT_ACL = "private"
+    STATICFILES_AWS_DEFAULT_ACL = "public-read"
+    FILE_OVERWRITE = False
+    AWS_S3_FILE_OVERWRITE = False
     AWS_QUERYSTRING_AUTH = False
-    # DO NOT change these unless you know what you're doing.
-    _AWS_EXPIRY = 60 * 60 * 24 * 7
-    # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+    _AWS_EXPIRY = 60 * 60 * 24 * 7  # 1 week in seconds
+
+    # Additional settings for controlling caching and object parameters
     AWS_S3_OBJECT_PARAMETERS = {
         "CacheControl": f"max-age={_AWS_EXPIRY}, s-maxage={_AWS_EXPIRY}, must-revalidate",
     }
-    # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
-    AWS_S3_MAX_MEMORY_SIZE = env.int(
-        "AWS_S3_MAX_MEMORY_SIZE",
-        default=100_000_000,  # 100MB
-    )
-    # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
-    AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME", default=None)
-    # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#cloudfront
-    AWS_S3_CUSTOM_DOMAIN = env("AWS_S3_CUSTOM_DOMAIN", default=None)
 
-    AWS_S3_HOST = (
-        AWS_S3_CUSTOM_DOMAIN
-        or f"{AWS_STORAGE_BUCKET_NAME} \
-        .s3.amazonaws.com"
-    )
+    # Maximum memory size for a single object
+    AWS_S3_MAX_MEMORY_SIZE = env.int("AWS_S3_MAX_MEMORY_SIZE", default=100_000_000)  # 100MB
 
-    MEDIA_URL = f"{AWS_S3_HOST}/media/"
-    STATIC_URL = f"{AWS_S3_HOST}/static/"
+    # Collectfast settings for faster static files management with Django and S3
     COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
-    STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-    AWS_DEFAULT_ACL = "private"
-    FILE_OVERWRITE = False
 
 elif TYPE_STORAGE == "gcp":
-    STORAGES = {
-        "default": {
-            "BACKEND": "storages.backends.gcloud.GoogleCloudStorage",
-            "OPTIONS": {
-                "location": "media",
-                "file_overwrite": False,
-            },
-        },
-        "staticfiles": {
-            "BACKEND": "storages.backends.gcloud.GoogleCloudStorage",
-            "OPTIONS": {
-                "location": "static",
-                "default_acl": "publicRead",
-            },
-        },
-    }
     GS_BUCKET_NAME = env("GCP_STORAGE_BUCKET_NAME")
-    GS_DEFAULT_ACL = "publicRead"
     MEDIA_URL = f"https://storage.googleapis.com/{GS_BUCKET_NAME}/media/"
-    COLLECTFAST_STRATEGY = "collectfast.strategies.gcloud.GoogleCloudStrategy"
     STATIC_URL = f"https://storage.googleapis.com/{GS_BUCKET_NAME}/static/"
+    COLLECTFAST_STRATEGY = "collectfast.strategies.gcloud.GoogleCloudStrategy"
+
+    STATICFILES_STORAGE = "storages.backends.gcloud.GoogleCloudStorage"
+    DEFAULT_FILE_STORAGE = "storages.backends.gcloud.GoogleCloudStorage"
+    GS_DEFAULT_ACL = "publicRead"
 
 elif TYPE_STORAGE == "local":
-    STATIC_URL = "static/"
     MEDIA_URL = "/media/"
+    STATIC_URL = "/static/"
     STATIC_ROOT = os.path.join(BASE_DIR, "static")
     MEDIA_ROOT = BASE_DIR / "static/media"
     COLLECTFAST_STRATEGY = "collectfast.strategies.filesystem.FileSystemStrategy"
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+    DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 
 else:
-    raise TypeError("Please specify the value of TYPE_STORAGE in the .env file")
-
+    raise ValueError("Invalid storage type specified: Please check the TYPE_STORAGE value in your .env file.")
