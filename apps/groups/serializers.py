d563c30ba8748eaa145eae0bc8a726e2ba6a137e
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-05-04 15:48:43 +0300

    add validations in groups

--- apps/groups/serializers.py
+++ apps/groups/serializers.py
@@ -13,13 +13,15 @@ class GroupSerializer(serializers.ModelSerializer):
         read_only_fields = ('id', 'created_on')
 
     def create(self, validated_data):
-        group_name = validated_data.pop('group')['name']
+        group_name = validated_data['group']['name'].strip()
+        if not group_name:
+            raise serializers.ValidationError({"group_name": "Group name cannot be empty."})
         group, created = Group.objects.get_or_create(name=group_name)
         group_metadata, _ = GroupMetadata.objects.get_or_create(group=group)
         return group_metadata
 
     def update(self, instance, validated_data):
-        group_name = validated_data.pop('group')['name']
+        group_name = validated_data.get('group', {}).get('name', '').strip()
         instance.group.name = group_name
         instance.group.save()
         return instance
