89155f62516b7c9e47dda5b972d7787798fefa3a
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-05-04 13:52:59 +0300

    update pagination

--- apps/utils/pagination.py
+++ apps/utils/pagination.py
@@ -3,4 +3,4 @@ from rest_framework.pagination import LimitOffsetPagination
 
 class SmallResultsSetPagination(LimitOffsetPagination):
     max_limit = 10
-    default_limit = 2
+    default_limit = 4
