7a4be168e7fef0a0365ad8c99a02d07f38262439
mojtaba <mojtaba.mohamed187@gmail.com>
2024-05-05 18:59:45 +0300

    Add two functions to handle the login and logout correctly, also added new state to check if the user is an admin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import { useState, useEffect, useMemo, createContext, useContext } from'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from'react-router-dom';
+import { useState, useEffect, useMemo, createContext, useContext } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import axios from 'axios';
 import Layout from './components/Layout';
 import Signup from './components/Signup';
@@ -10,11 +10,13 @@ import ActivationPage from './components/ActivationPage';
 import EmailUploader from './components/EmailUploader';
 import Policies from './components/Policies';
 import CreateCampaign from './components/CreateCampaign';
+import PolicyDetails from './components/PolicyDetails';
+import CreatePolicy from './components/CreatePolicy';
 
 // Create a context for the user's authentication state
 export const AuthContext = createContext();
 
-const useAuth = () => useContext(AuthContext);
+export const useAuth = () => useContext(AuthContext);
 
 axios.interceptors.request.use(config => {
   const token = localStorage.getItem('authToken');
@@ -36,12 +38,26 @@ const ProtectedRoute = ({ children }) => {
 
 function App() {
 
+  const [isAdmin, setIsAdmin] = useState(false);
   const [username, setUsername] = useState('');
   const [authToken, setAuthToken] = useState(localStorage.getItem('authToken'));
 
   const headers = useMemo(() => ({
     'Authorization': `Token ${authToken}`
   }), [authToken]);
+
+  // This function is called upon user login to update the authToken and fetch the username
+  const handleLogin = (token) => {
+    setAuthToken(token);
+    localStorage.setItem('authToken', token);
+  };
+
+  // This function is called upon user logout to clear authToken and username
+  const handleLogout = () => {
+    localStorage.removeItem('authToken');
+    setAuthToken(null);
+    setUsername(''); // Reset username to empty string
+  };
 
   useEffect(() => {
     const fetchUsername = async () => {
@@ -50,52 +66,71 @@ function App() {
       try {
         const response = await axios.get('http://127.0.0.1:8000/api/auth/users/me/', { headers });
         setUsername(response.data.email);
+        if (response.data.is_superuser === true) {
+          setIsAdmin(true);
+        } else {
+          setIsAdmin(false);
+        }
       } catch (error) {
         console.error('Error fetching user data:', error);
-        // Handle error, e.g., log out the user or show an error message
         setAuthToken(null);
         localStorage.removeItem('authToken');
       }
     };
 
     fetchUsername();
+    console.log('Fetching username...');
   }, [authToken, headers]);
 
-  // Provide the authentication state via context
-  const authContextValue = { authToken, setAuthToken };
+
+  const authContextValue = { authToken, setAuthToken, handleLogin, isAdmin, handleLogout };
 
   return (
     <AuthContext.Provider value={authContextValue}>
       <Router>
-      <Routes>
+        <Routes>
           <Route path="/signup" element={<Signup />} />
           <Route path="/auth/activate" element={<ActivationPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Navigate replace to="/login" />} />
           <Route path='/home' element={
             <ProtectedRoute>
-              <Layout username={username}>
+              <Layout username={username} handleLogout={handleLogout}>
                 <Home />
               </Layout>
             </ProtectedRoute>
           } />
           <Route path='/emailuploader' element={
             <ProtectedRoute>
-              <Layout username={username}>
+              <Layout username={username} handleLogout={handleLogout}>
                 <EmailUploader />
               </Layout>
             </ProtectedRoute>
           } />
           <Route path='/policies' element={
             <ProtectedRoute>
-              <Layout username={username}>
+              <Layout username={username} handleLogout={handleLogout}>
                 <Policies />
+              </Layout>
+            </ProtectedRoute>
+          } />
+          <Route path='/policies/:id' element={
+            <ProtectedRoute>
+              <Layout username={username} handleLogout={handleLogout}>
+                <PolicyDetails />
+              </Layout>
+            </ProtectedRoute>
+          } />
+          <Route path='/create-policy' element={
+            <ProtectedRoute>
+              <Layout username={username} handleLogout={handleLogout}>
+                <CreatePolicy />
               </Layout>
             </ProtectedRoute>
           } />
           <Route path='/create-campaign' element={
             <ProtectedRoute>
-              <Layout username={username}>
+              <Layout username={username} handleLogout={handleLogout}>
                 <CreateCampaign />
               </Layout>
             </ProtectedRoute>
