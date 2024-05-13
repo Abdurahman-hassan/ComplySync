e33c6b1489b2a586d124c2b6dd344cd35b8197b1
mojtaba <mojtaba.mohamed187@gmail.com>
2024-05-13 17:12:33 +0300

    Upgrade nth-check

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 import { useAuth } from "./App";
 import { useNavigate } from "react-router-dom";
