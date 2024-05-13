98277cc214df44af252a88fb0b1a4086d7b9f5b1
mojtaba <mojtaba.mohamed187@gmail.com>
2024-05-13 17:02:24 +0300

    add previuos and next page buttons

--- frontend/src/components/Documents.js
+++ frontend/src/components/Documents.js
@@ -1,50 +1,89 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../App';
+import  '../styles/Documents.css';
+import { formatDate } from '../utils';
 
 const Documents = () => {
 
     const navigate = useNavigate();
-    const { isAdmin } = useAuth();
+    const { authToken, isAdmin } = useAuth();
     const [documents, setDocuments] = useState([]);
+    const [nextPage, setNextPage] = useState(null);
+    const [prevPage, setPrevPage] = useState(null);
+
+    const fetchDocuments = useCallback(async (url) => {
+
+        try {
+            const response = await axios.get(url, {
+                headers: { 'Authorization': `Token ${authToken}` }
+            });
+            console.log(response.data);
+
+            setDocuments(response.data.results);
+            setNextPage(response.data.next); // Save the next page URL
+            setPrevPage(response.data.previous); // Save the previous page URL
+        } catch (error) {
+            console.error('Error fetching documents:', error);
+        }
+
+    }, [authToken]);
+
 
     useEffect(() => {
-        const fetchDocuments = async () => {
-            try {
-                const response = await axios.get('http://127.0.0.1:8000/api/languages/');
-                setDocuments(response.data.results);
-            } catch (error) {
-                console.error('Error fetching documents:', error);
-            }
-        };
+        fetchDocuments('http://127.0.0.1:8000/api/languages/');
+    }, [fetchDocuments]);
 
-        fetchDocuments();
-    }, []);
-
-
-    const handleUploadDocument = () => {
-        navigate('/upload-pdf');
+    const handleNextPage = () => {
+        if (nextPage) {
+            fetchDocuments(nextPage);
+        }
     };
 
-    const handleDocumentClick = (groupId) => {
-        navigate(`/documents/${document.id}`);
+    const handlePrevPage = () => {
+        if (prevPage) {
+            fetchDocuments(prevPage);
+        }
+    };
+
+    const handleUploadDocument = async () => {
+        const url = 'http://127.0.0.1:8000/api/upload-pdf/';
+        window.open(url, '_blank');
     };
 
     return (
-        <div>
-            <h1>Documents</h1>
-
-            {documents.map(document => (
-                <div key={document.id} className='document-item' onClick={() => handleDocumentClick(document.id)}>
-                    <h3>{document.name}</h3>
+        <div className='documents-list'>
+            <div className="documents-header">
+                <h2>Documents</h2>
+                { isAdmin && (
+                <button onClick={handleUploadDocument} className='create-policy-button'>
+                    Create Policy
+                </button>
+            ) }
+            </div>
+            {documents.length > 0 ? (
+                documents.map((document, index) => (
+                    <div key={index} onClick={() => navigate(`/documents/${document.id}`)} className='document-item'>
+                        <h3>{document.localized_title}</h3>
+                        <p>{formatDate(document.last_updated)}</p>
+                    </div>
+                ))
+            ) : (
+                <div className="no-documents-message">
+                    <h3>There are no documents now, enjoy the silence.</h3>
                 </div>
-            ))}
-            {isAdmin && (
-                <button onClick={handleUploadDocument} >Upload New Document</button>
             )}
+            <div className='pagination-buttons'>
+                {prevPage && (
+                    <button className='prev-page-button' onClick={handlePrevPage}>Previous Page</button>
+                )}
+                {nextPage && (
+                    <button className='next-page-button' onClick={handleNextPage}>Next Page</button>
+                )}
+            </div>
         </div>
     );
-}
+};
 
 export default Documents;
