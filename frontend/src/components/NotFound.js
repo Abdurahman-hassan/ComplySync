8d44a37ac0683f7974643d010a933c1bde8adafa
mojtaba <mojtaba.mohamed187@gmail.com>
2024-05-15 13:11:29 +0300

    style Not Found page

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
+import '../styles/NotFound.css';
 
 const NotFound = () => {
     return (
         <div className="not-found">
             <h2>Sorry</h2>
             <p>That page cannot be found</p>
-            <Link to='/'>Back to homepage....</Link>
+            <Link to='/'>Back to homepage</Link>
         </div>
     );
 }
