650dfbf4dc7df6793ef23b9fd005a77ccd895509
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-05-19 07:00:24 +0300

    add CORS_ORIGIN_WHITELIST, CORS_ALLOWED_ORIGINS

--- config/settings/development.py
+++ config/settings/development.py
@@ -2,7 +2,6 @@ from .base import *  # noqa: F403
 from .base import INSTALLED_APPS, MIDDLEWARE
 from config.utils.storages import *  # noqa: F403
 
-
 ADMIN_URL = "admin"
 # === DEBUG Tools ================================================
 DEBUG = True
@@ -24,5 +23,22 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
+CORS_ALLOWED_ORIGINS = [
+    'http://api.greencoder.tech',
+    'https://api.greencoder.tech',
+    'http://complysync.greencoder.tech:3000'
+    'https://complysync.greencoder.tech',
+    'https://www.greencoder.tech',
+    'https://greencoder.tech',
+]
 
+CORS_ORIGIN_ALLOW_ALL = False
 
+CORS_ORIGIN_WHITELIST = [
+    'http://api.greencoder.tech',
+    'https://api.greencoder.tech',
+    'http://complysync.greencoder.tech:3000'
+    'https://complysync.greencoder.tech',
+    'https://www.greencoder.tech',
+    'https://greencoder.tech'
+]
